32884f94e928c6195447a8a838cadb57c8539939
Ruby Weiner <rubyweiner796@me.com>
2018-09-06 16:12:20 -0400

    fridgecontainer component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FridgeContainer from './FridgeContainer'
 
 class App extends Component {
+  state = {
+    groceries: []
+  }
+
+  fetchGroceries = () => {
+  
+  }
+
+  componentDidMount() {
+    this.fetchGroceries()
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FridgeContainer groceries={this.state.groceries}/>
       </div>
     );
   }
